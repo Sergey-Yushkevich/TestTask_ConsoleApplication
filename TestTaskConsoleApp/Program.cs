2109c1f85fc2ac01fcb3cec1d15d0e1035be068e
Sergey <serge.juskevic@gmail.com>
2023-05-08 12:37:49 +0300

    Add implimintation for read file, sorting and filtration by date

---
+++ TestTaskConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace TestTaskConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static IEnumerable<Item> GetItems(string path)
+        {
+            XDocument document = XDocument.Load(path);
+
+            var polytic = document.Element("channel")?
+                .Elements("item")
+                .Where(i => (i.Element("category")?.Value).Contains("Политика"))
+                .Select(i => new Item
+                {
+                    Title = i.Element("title")?.Value,
+                    Link = i.Element("link")?.Value,
+                    Description = i.Element("description")?.Value,
+                    Category = i.Element("category")?.Value,
+                    PubDate = DateTime.Parse(i.Element("pubDate")?.Value, CultureInfo.CreateSpecificCulture("en-US"))
+                })?
+                .OrderBy(i => i.PubDate);
+
+            return polytic;
+        }
+    }
+}
